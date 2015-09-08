ef586e844ba085f0e640dc5a9a0b43449124e866
Dave Moten <davidmoten@gmail.com>
2015-09-08 12:29:41 +1000

    initial commit

--- src/main/java/com/github/davidmoten/graph/Main.java
+++ src/main/java/com/github/davidmoten/graph/Main.java
@@ -10,20 +10,6 @@ import edu.uci.ics.jung.graph.util.EdgeType;
 public class Main {
 
     public static void main(String[] args) {
-        // Graph<V, E> where V is the type of the vertices
-        // and E is the type of the edges
-        {
-            Graph<Integer, String> g2 = new SparseMultigraph<Integer, String>();
-            g2.addVertex(1);
-            g2.addVertex(2);
-            g2.addVertex(3);
-            g2.addEdge("Edge-A", 1, 3);
-            g2.addEdge("Edge-B", 2, 3, EdgeType.DIRECTED);
-            g2.addEdge("Edge-C", 3, 2, EdgeType.DIRECTED);
-            g2.addEdge("Edge-P", 2, 3); // A parallel edge
-            System.out.println("The graph g2 = " + g2.toString());
-        }
-
         // Graph<String, Number> g = TestGraphs.getOneComponentGraph();
         Graph<String, String> g = createTestGraph();
 
